7c3dc3a5a8f9b86cc7ca9cac1167269e388ef541
sxidsvit <s.p.antonyuk@gmail.com>
2018-04-29 21:56:10 +0300

    top line created

---
+++ src/assets/js/main.js
@@ -0,0 +1,13 @@
+//= ../../../node_modules/jquery/dist/jquery.js
+//= ../../../node_modules/popper.js/dist/umd/popper.js
+//= ../../../node_modules/bootstrap/js/dist/util.js
+//= ../../../node_modules/bootstrap/js/dist/tooltip.js
+//= ../../../node_modules/bootstrap/js/dist/popover.js
+//= ../../../node_modules/bootstrap/js/dist/collapse.js
+//= ../../../node_modules/bootstrap/js/dist/dropdown.js
+
+//= ../../../node_modules/pixel-glass/script.js
+
+//= custom/fontawesome-all.js
+//= custom/tooltip.js
+//= custom/popover.js
